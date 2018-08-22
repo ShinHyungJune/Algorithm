797a6923ecc194f788dc8521b407b6ee4e6da383
HOME_신형준 <ssa4141@naver.com>
2018-08-22 23:51:44 +0900

    큐

---
+++ class/Queue.php
@@ -0,0 +1,49 @@
+<?php
+class Queue
+{
+    public $arr = [];
+    public $indexOfFront = 0;
+    public $indexOfBack = 0;
+
+    public function push($value)
+    {
+        $this->arr[$this->indexOfBack] = $value;
+        $this->indexOfBack++;
+    }
+
+    public function pop()
+    {
+        if($this->empty())
+            return -1;
+
+        $value = $this->arr[$this->indexOfFront++];
+        return $value;
+    }
+
+    public function size()
+    {
+        return $this->indexOfBack - $this->indexOfFront;
+    }
+
+    public function empty()
+    {
+        if($this->size() == 0)
+            return 1;
+        return 0;
+    }
+
+    public function front()
+    {
+        if($this->empty())
+            return -1;
+        return $this->arr[$this->indexOfFront];
+    }
+
+    public function back()
+    {
+        if($this->empty())
+            return -1;
+        return $this->arr[$this->indexOfBack - 1];
+    }
+}
+?>
